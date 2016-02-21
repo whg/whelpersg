cefe3f2606ca6b21e9f86996873846696beef63a
Will Gallia <wgallia@gmail.com>
2016-02-21 15:49:30 +0000

    make midimapper a singleton

--- ofxMidiMapper.cpp
+++ ofxMidiMapper.cpp
@@ -3,3 +3,14 @@
 
 bool ofxMidiMapper::learning = false;
 ofxBaseGui* ofxMidiMapper::selectedGui = nullptr;
+
+
+shared_ptr<ofxMidiMapper> midiMapper = nullptr;
+const int DEVICE_POLLING_INTERVAL = 5; // seconds
+
+ofxMidiMapper& ofxMidiMapper::get() {
+    if (midiMapper == nullptr) {
+        midiMapper = shared_ptr<ofxMidiMapper>(new ofxMidiMapper);
+    }
+    return *midiMapper.get();
+}
--- ofxMidiMapper.h
+++ ofxMidiMapper.h
@@ -8,6 +8,8 @@ class ofxMidiMapper : public ofxMidiListener {
 
 public:
     
+    static ofxMidiMapper& get();
+    
     void setup(ofxMidiIn &midiIn) {
         midiIn.addListener(this);
         ofAddListener(ofxBaseGui::guiSelectedEvent, this, &ofxMidiMapper::guiSelected);
@@ -20,32 +22,55 @@ public:
 
     void newMidiMessage(ofxMidiMessage& msg) {
         if (ofxMidiMapper::learning && ofxMidiMapper::selectedGui != nullptr) {
+            auto *abstractParam = &selectedGui->getParameter();
             if (msg.status == MIDI_CONTROL_CHANGE) {
-                mParameterMap[msg.control].push_back(&selectedGui->getParameter());
+                mParameterControlMap[msg.control].push_back(abstractParam);
                 ofxMidiMapper::selectedGui = nullptr;
             }
             else if (msg.status == MIDI_NOTE_ON) {
-                auto &param = selectedGui->getParameter();
-                auto type = param.type();
+                mParameterNoteMap[msg.pitch].push_back(abstractParam);
                 
-                if (type == typeid(ofParameter<int>).name()) {
-                    auto intParam = param.cast<int>();
-                    intParam.set(msg.pitch);
+                if (mInvertedNoteIndex.count(abstractParam)) {
+                    int note = mInvertedNoteIndex[abstractParam];
+                    auto &params = mParameterNoteMap[note];
+                    for (auto it = params.begin(); it != params.end(); ++it) {
+                        if (*it == abstractParam) {
+                            params.erase(it);
+                            break;
+                        }
+                    }
                 }
-                else if (type == typeid(ofParameter<float>).name()) {
-                    auto floatParam = param.cast<float>();
-                    floatParam.set(msg.pitch);
-                }
+                
+                mInvertedNoteIndex[abstractParam] = msg.pitch;
+                
                 ofxMidiMapper::selectedGui = nullptr;
-
             }
+//            
+//                auto &param = selectedGui->getParameter();
+//                auto type = param.type();
+//                
+//                if (type == typeid(ofParameter<int>).name()) {
+//                    auto intParam = param.cast<int>();
+//                    intParam.set(msg.);
+//                }
+//                else if (type == typeid(ofParameter<float>).name()) {
+//                    auto floatParam = param.cast<float>();
+//                    floatParam.set(msg.pitch);
+//                }
+//                else if (type == typeid(ofParameter<unsigned char>).name()) {
+//                    auto ucharParam = param.cast<unsigned char>();
+//                    ucharParam.set(ofMap(msg.value, 0, 127, ucharParam.getMin(), ucharParam.getMax()));
+//                }
+//                ofxMidiMapper::selectedGui = nullptr;
+//
+//            }
         }
         
         else {
             if (msg.status == MIDI_CONTROL_CHANGE) {
-                if (mParameterMap.count(msg.control)) {
+                if (mParameterControlMap.count(msg.control)) {
                 
-                    for (auto *param : mParameterMap[msg.control]) {
+                    for (auto *param : mParameterControlMap[msg.control]) {
                     
                         auto type = param->type();
 
@@ -56,6 +81,36 @@ public:
                         else if (type == typeid(ofParameter<float>).name()) {
                             auto floatParam = param->cast<float>();
                             floatParam.set(ofMap(msg.value, 0, 127, floatParam.getMin(), floatParam.getMax()));
+                        }
+                        else if (type == typeid(ofParameter<unsigned char>).name()) {
+                            auto ucharParam = param->cast<unsigned char>();
+                            ucharParam.set(ofMap(msg.value, 0, 127, ucharParam.getMin(), ucharParam.getMax()));
+                        }
+                    }
+                    
+                }
+            }
+            else if (msg.status == MIDI_NOTE_ON || msg.status == MIDI_NOTE_OFF || msg.status == MIDI_POLY_AFTERTOUCH) {
+                if (mParameterNoteMap.count(msg.pitch)) {
+                    if (msg.status == MIDI_POLY_AFTERTOUCH) {
+                        msg.velocity = msg.value;
+                    }
+                    
+                    for (auto *param : mParameterNoteMap[msg.pitch]) {
+                        
+                        auto type = param->type();
+    
+                        if (type == typeid(ofParameter<int>).name()) {
+                            auto intParam = param->cast<int>();
+                            intParam.set(ofMap(msg.velocity, 0, 127, intParam.getMin(), intParam.getMax()));
+                        }
+                        else if (type == typeid(ofParameter<float>).name()) {
+                            auto floatParam = param->cast<float>();
+                            floatParam.set(ofMap(msg.velocity, 0, 127, floatParam.getMin(), floatParam.getMax()));
+                        }
+                        else if (type == typeid(ofParameter<unsigned char>).name()) {
+                            auto ucharParam = param->cast<unsigned char>();
+                            ucharParam.set(ofMap(msg.velocity, 0, 127, ucharParam.getMin(), ucharParam.getMax()));
                         }
                     }
                     
@@ -78,6 +133,9 @@ public:
     static ofxBaseGui *selectedGui;
     
 protected:
-    map<int, vector<ofAbstractParameter*>> mParameterMap;
+    map<int, vector<ofAbstractParameter*>> mParameterControlMap;
+    map<int, vector<ofAbstractParameter*>> mParameterNoteMap;
     
+    map<ofAbstractParameter*, int> mInvertedNoteIndex;
+    map<ofAbstractParameter*, int> mInvertedControlIndex;
 };
