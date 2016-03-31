253c1999113b97e6cbe7ef7a413285a98d1c9bcc
Will Gallia <wgallia@gmail.com>
2016-03-31 15:10:47 +0100

    onset detector cleaned

--- onset.h
+++ onset.h
@@ -17,8 +17,7 @@ class OnsetDetector {
 	const long SLEEP_TICKS = 12;
 
 public:
-	OnsetDetector(): mSleepCounter(0), mAverageDiff(0), mLastValue(0),
-					 mCurrentState(IDLE), mGainThreshold(0.25) {
+	OnsetDetector(): mSleepCounter(0), mAverageDiff(0), mLastValue(0), mGainThreshold(0.25) {
 		
 		mHistory.setCapacity(64);
 	}
@@ -28,19 +27,13 @@ public:
 		T diff = value - mLastValue;
 		mLastValue = value;
 		mCurrentState = false;
-		
-//		if (diff < 0 && mState == ARMED) {
-//			mCurrentState = true;
-//			mState = IDLE;
-//		}
-		
+
 		if (value > mGainThreshold && diff > mAverageDiff * DIFF_MARGIN && mSleepCounter <= 0) {
 			mCurrentState = true;
 			mSleepCounter = SLEEP_TICKS;
-//			mState = ARMED;
 		}
 		
-		if (diff > 0 && mState == IDLE) {
+		if (diff > 0) {
 			
 			mHistory.push(diff);
 			mAverageDiff = whg::mean(mHistory.data());
@@ -61,10 +54,6 @@ protected:
 	bool mCurrentState;
 	T mGainThreshold;
 	
-	enum State {
-		IDLE, ARMED,
-	};
-	State mState;
 };
 
 
@@ -79,7 +68,6 @@ public:
 		
 		mHistory.setCapacity(512);
 		mIntervalCounter.setCapacity(32);
-		mSecondLevelFilter.setCapacity(24);
 	}
 	
 	float update(T value) {
@@ -88,54 +76,24 @@ public:
 		
 		mHistory.push(value);
 		
+		// autocorrelation via FFT
 		mFFT->forward(mHistory.data().begin(), mHistory.data().end());
 		mFFT->inverse(mFFT->getPower());
 		const auto &ac = mFFT->getInput();
-		
-//		using ::operator<<;
-//		auto q = vector<T>(mHistory.data().begin(),mHistory.data().end());
-//		cout << q << endl;
-		
+
+		// + 1 as we never will want the DC component
 		auto sorted = whg::argsort(ac.begin()+1, ac.end());
 		for (int i = sorted.size()-6; i < sorted.size(); i++) {
-//			std::cout << sorted[i] << "(" << binToBpm(sorted[i]+1) << ")" << ", ";
 			auto rbpm = (binToBpm(sorted[i]+1));
 			if (rbpm > 60 && rbpm < 180) {
 				
 				mIntervalCounter.increment(smoothBpm(rbpm));
 			}
 		}
-//		std::cout << std::endl;
-//
 
-//		float bpm;
-//		for (int i = sorted.size() - 1; i >= 0; i--) {
-//			bpm = binToBpm(sorted[i]+1);
-//			if (bpm > 60 && bpm < 165) {
-//				break;
-//			}
-//		}
-//		std::cout << bpm << std::endl;
-		
-		// +1 because the DC is of no use to us
-//		auto maxBin = whg::argmax(ac.begin()+1, ac.end()) + 1;
-//		cout << "- " << maxBin << endl;
-//		float hopTime = mHopSize / static_cast<float>(sampleRate);
-//		auto rbpm = reasonableBpm(binToBpm(maxBin));
-//		cout << bpm << endl;
-//		mIntervalCounter.increment(rbpm);
 		mCurrentBpm = mIntervalCounter.getMaxKey();
-//		std::cout << mIntervalCounter.data() << std::endl;
-//		std::cout << modeBpm << std::endl;
 		
-//		mSecondLevelFilter.increment(modeBpm);
-//		mBpmCounter.increment(rbpm);
-//		
-//		std::cout << mBpmCounter.data() << std::endl;
-		
-		return mCurrentBpm; //mIntervalCounter.getMaxKey();
-//		std::cout << maxBin << std::endl;
-//		return 60.0f / ((mHopSize / static_cast<float>(sampleRate)) * modeBin);
+		return mCurrentBpm;
 	}
 	
 	void setHopSize(size_t hs) { mHopSize = hs; }
@@ -144,43 +102,22 @@ public:
 protected:
 	size_t mHopSize;
 	std::unique_ptr<dsp::RealFFT> mFFT;
-//	std::deque<T> mHistory;
 	whg::HistoryQueue<T> mHistory;
 	whg::IntervalCounter<float> mIntervalCounter;
-	whg::IntervalCounter<float> mSecondLevelFilter;
-	whg::Counter<float> mBpmCounter;
 	
 	bpmType mCurrentBpm;
 	
 	bpmType binToBpm(size_t binNum) const {
 
-		return 60.0f / ((mHopSize / static_cast<float>(sampleRate)) * binNum);
-	}
-	
-	bpmType reasonableBpm(bpmType bpm) const {
-
-		if (bpm < 0 || bpm > 1e6) return 0;
-	
-		while (bpm < 60) {
-			bpm*= 2;
-		}
-		
-		while (bpm > 164) {
-			bpm/= 2;
-		}
-		return bpm;
+		return 60.0f / (mHopSize / static_cast<float>(sampleRate) * binNum);
 	}
 	
 	bpmType smoothBpm(bpmType bpm) {
 		bpmType doubleBpm = bpm * 2;
-		bpmType halfBpm = bpm * 0.5;
 		static const bpmType margin = 1.5;
 		if (doubleBpm < mCurrentBpm + margin && doubleBpm > mCurrentBpm - margin) {
 			return mCurrentBpm;
 		}
-//		else if (halfBpm < mCurrentBpm + margin && halfBpm > mCurrentBpm - margin) {
-//			return bpm;
-//		}
 		return bpm;
 	}
 	
