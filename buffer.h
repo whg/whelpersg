a5501616a5bdcc0af1d72f03b63a9dc8621bb58a
Will Gallia <wgallia@gmail.com>
2016-04-01 18:13:20 +0100

    add atomicSingleQueue, common sublass for buffers

--- buffer.h
+++ buffer.h
@@ -2,29 +2,36 @@
 
 #include <vector>
 #include <algorithm>
+#include <atomic>
 
 template <typename T>
-class RingBuffer {
+struct BaseData {
+	std::vector<T> data;
+	size_t mSize;
+};
+
+template <typename T>
+class RingBuffer : public BaseData<T> {
 public:
 	RingBuffer(size_t size=1024): mHead(0), mTail(0) {
 		resize(size);
 	}
 	
 	void resize(size_t n) {
-		mSize = n;
-		data.resize(mSize);
+		this->mSize = n;
+		this->data.resize(this->mSize);
 	}
 	
 	size_t size() const {
 		auto tail = mTail;
 		if (tail < mHead) {
-			tail+= mSize;
+			tail+= this->mSize;
 		}
 		return tail - mHead;
 	}
 	
 	size_t capacity() const {
-		return mSize;
+		return this->mSize;
 	}
 	
 	size_t available() const {
@@ -37,44 +44,39 @@ public:
 	
 	bool push(T v) {
 		if (available() >= 2) {
-			data[mTail] = v;
-			mTail = (mTail + 1) % mSize;
+			this->data[mTail] = v;
+			mTail = (mTail + 1) % this->mSize;
 			return true;
 		}
 		return false;
 	}
 	
 	T peek(size_t offset=0) const {
-		return data[(mHead + offset) % mSize];
+		return this->data[(mHead + offset) % this->mSize];
 	}
 	
 	T pop() {
-		T temp = data[mHead];
-		mHead = (mHead + 1) % mSize;
+		T temp = this->data[mHead];
+		mHead = (mHead + 1) % this->mSize;
 		return temp;
 	}
 	
 	void discard(size_t n=1) {
 		// TODO: raise an exception if we try to discard too much?
-		auto nextHead = (mHead + n) % mSize;
-//		if (nextHead <= mTail) {
-			mHead = nextHead;
-//		}
-//		else {
-//			std::cout << "asdfasd" << std::endl;
-//		}
+		auto nextHead = (mHead + n) % this->mSize;
+		mHead = nextHead;
 	}
 	
 	bool push(const T* const vs, size_t N) {
 		if (available() >= N + 1) {
-			auto firstChunk = std::min(N, mSize - mTail);
+			auto firstChunk = std::min(N, this->mSize - mTail);
 			
-			std::copy(vs, vs + firstChunk, &data[mTail]);
-			mTail = (mTail + firstChunk) % mSize;
+			std::copy(vs, vs + firstChunk, &this->data[mTail]);
+			mTail = (mTail + firstChunk) % this->mSize;
 			
 			if (firstChunk != N) {
-				std::copy(vs + firstChunk, vs + N, &data[mTail]);
-				mTail = (mTail + (N - firstChunk)) % mSize;
+				std::copy(vs + firstChunk, vs + N, &this->data[mTail]);
+				mTail = (mTail + (N - firstChunk)) % this->mSize;
 			}
 			return true;
 		}
@@ -83,18 +85,6 @@ public:
 	
 	bool push(const std::vector<T> &vs) {
 		return push(&vs[0], vs.size());
-//		if (available() >= vs.size() + 1) {
-//			auto firstChunk = std::min(vs.size(), mSize - mTail);
-//			
-//			std::copy(vs.begin(), vs.begin() + firstChunk, &data[mTail]);
-//			mTail = (mTail + firstChunk) % mSize;
-//			
-//			if (firstChunk != vs.size()) {
-//				std::copy(vs.begin() + firstChunk, vs.end(), &data[mTail]);
-//			}
-//			return true;
-//		}
-//		return false;
 	}
 	
 	
@@ -109,17 +99,69 @@ public:
 	}
 	
 protected:
-	std::vector<T> data;
-	size_t mHead, mTail, mSize;
+	size_t mHead, mTail;
 	
 	friend std::ostream& operator<<(std::ostream &os, RingBuffer<T> const &rb);
 };
 
-//template<typename T>
-inline std::ostream& operator<<(std::ostream &os, RingBuffer<float> const &rb) {
+template<typename T>
+inline std::ostream& operator<<(std::ostream &os, BaseData<T> const &bd) {
 	os << "[";
-	for (int i =0 ; i < rb.data.size(); i++) {
-		os << rb.data[i] << (i == (rb.data.size()-1) ? "" : ", ");
+	for (int i =0 ; i < bd.data.size(); i++) {
+		os << bd.data[i] << (i == (bd.data.size()-1) ? "" : ", ");
 	}
 	return os << "]";
 }
+
+template<typename T>
+class AtomicSingleQueue : protected BaseData<T> {
+public:
+	
+	AtomicSingleQueue(size_t size): mHead(0), mTail(0) {
+		this->mSize = size;
+		this->data.resize(size);
+	}
+	
+	bool push(T &&value) {
+		auto writePos = mTail.load();
+		auto nextWritePos = (writePos + 1) % this->mSize;
+		
+		if (nextWritePos != mHead.load()) {
+			this->data[writePos] = value;
+			mTail.store(nextWritePos);
+			return true;
+		}
+		return false;
+	}
+	
+	bool pop(T &output) {
+		auto readPos = mHead.load();
+		if (readPos == mTail.load()) {
+			return false;
+		}
+		
+		auto nextReadPos = (readPos + 1) % this->mSize;
+		
+		output = std::move(this->data[readPos]);
+		mHead.store(nextReadPos);
+		return true;
+	}
+	
+	bool empty() {
+		return mHead.load() == mTail.load();
+	}
+	
+	size_t size() const {
+		auto tail = mTail.load();
+		auto head = mHead.load();
+		if (tail < head) {
+			tail+= this->mSize;
+		}
+		return tail - head;
+	}
+	
+protected:
+
+	std::atomic<size_t> mHead, mTail;
+
+};
