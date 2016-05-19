00d6ce8006b9edda38420def35ef39bccffe61eb
Will Gallia <wgallia@gmail.com>
2016-05-19 12:53:25 +0100

    namespace timing

--- timing.h
+++ timing.h
@@ -1,24 +1,25 @@
 #include <chrono>
-using namespace std::chrono;
+
+namespace whg {
 
 template<class T>
 struct _Timer {
 	// we're using time_points and durations because VS can't do arithmetic on durations
-	time_point<system_clock> startTime;
+    std::chrono::time_point<std::chrono::system_clock> startTime;
 	T timeTaken;
 
 	void start() {
-		startTime = system_clock::now();
+		startTime = std::chrono::system_clock::now();
 	}
 
 	void end() {
-		timeTaken = duration_cast<T>(system_clock::now() - startTime);
+		timeTaken = std::chrono::duration_cast<T>(std::chrono::system_clock::now() - startTime);
 	}
 };
 
-using Timer = _Timer<seconds>;
-using SecondsTimer = _Timer<seconds>;
-using MillisTimer = _Timer<milliseconds>;
+using Timer = _Timer<std::chrono::seconds>;
+using SecondsTimer = _Timer<std::chrono::seconds>;
+using MillisTimer = _Timer<std::chrono::milliseconds>;
 
 template<class T>
 struct _ScopedTimer : public T {
@@ -26,14 +27,17 @@ struct _ScopedTimer : public T {
 	_ScopedTimer() = delete; // ScopedTimers must have names
 	_ScopedTimer(std::string n) {
 		name = n;
-		start();
+		T::start();
 	}
 
 	~_ScopedTimer() {
-		end();
-		std::cout << name << " took " << timeTaken.count() << "ms" << std::endl;
+		T::end();
+		std::cout << name << " took " << T::timeTaken.count() << "ms" << std::endl;
 	}
 };
 
 using ScopedTimer = _ScopedTimer<MillisTimer>;
 using ScopedSecondsTimer = _ScopedTimer<SecondsTimer>;
+
+
+}
