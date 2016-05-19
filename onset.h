b435bbb008a448c83800d0c177d06d68f3b46366
Will Gallia <wgallia@gmail.com>
2016-05-19 12:54:10 +0100

    optimise tempoEstimator a bit

--- onset.h
+++ onset.h
@@ -14,7 +14,7 @@ template<typename T>
 class OnsetDetector {
 	
 	const float DIFF_MARGIN = 2.0f;
-	const long SLEEP_TICKS = 12;
+	const long SLEEP_TICKS = 24;
 
 public:
 	OnsetDetector(): mSleepCounter(0), mAverageDiff(0), mLastValue(0), mGainThreshold(0.25) {
@@ -63,8 +63,8 @@ class TempoEstimator {
 public:
 
 	using bpmType = float;
-
-	TempoEstimator(): mHopSize(512), mCurrentBpm(120) {
+	
+	TempoEstimator(): mHopSize(512), mCurrentBpm(120), mFFTPower(512, 0) {
 		mFFT = std::unique_ptr<dsp::RealFFT>(new dsp::RealFFT(512));
 		
 		mHistory.setCapacity(512);
@@ -75,20 +75,49 @@ public:
 		
 		mHistory.push(value);
 		
+		if (mHistory.data().size() != 512) return 0;
+		
 		// autocorrelation via FFT
 		mFFT->forward(mHistory.data().begin(), mHistory.data().end());
-		mFFT->inverse(mFFT->getPower());
+		mFFT->getPower(mFFTPower);
+		mFFT->inverse(mFFTPower);
 		const auto &ac = mFFT->getInput();
 
 		// + 1 as we never will want the DC component
 		auto sorted = whg::argsort(ac.begin()+1, ac.end());
-		for (int i = sorted.size()-6; i < sorted.size(); i++) {
+        for (size_t i = sorted.size(); --i > sorted.size() - 6; ) {
 			auto rbpm = (binToBpm(sorted[i]+1));
 			if (rbpm > 69 && rbpm < 180) {
 				
 				mIntervalCounter.increment(smoothBpm(rbpm));
+                mFoundValue = ac[sorted[i]+1];
+                break;
 			}
 		}
+
+//        auto localmaxes = whg::localmax(ac.begin(), ac.end());
+//		auto sorted = whg::argsort(ac.begin(), ac.end());
+//        for (size_t i = sorted.size(); --i > 0; ) {
+//            size_t index = sorted[i];
+//            if (localmaxes[index]) {
+//                std::vector<float> mults = { 1.0f / 3.0f, 0.5f, 1.0f, 2.0f, 3.0f };
+//                float maxval = 0.0f;
+//                size_t maxbin = 0;
+//                for (auto mult : mults) {
+//                    size_t bin = static_cast<size_t>(index * mult);
+//                    if (bin < 511) {
+//                        if (ac[bin] > maxval) {
+//                            maxbin = bin;
+//                            maxval = ac[bin];
+//                        }
+//                    }
+//                }
+//                auto bpm = binToBpm(maxbin);
+//                if (bpm > 69 && bpm < 180) {
+//                    mIntervalCounter.increment(smoothBpm(bpm));
+//                }
+//            }
+//        }
 
 		mCurrentBpm = mIntervalCounter.getMaxKey();
 		
@@ -104,6 +133,7 @@ protected:
 	std::unique_ptr<dsp::RealFFT> mFFT;
 	whg::HistoryQueue<T> mHistory;
 	whg::IntervalCounter<float> mIntervalCounter;
+	std::vector<float> mFFTPower;
 	
 	bpmType mCurrentBpm;
 	
@@ -120,6 +150,9 @@ protected:
 		}
 		return bpm;
 	}
+    
+public:
+    float mFoundValue;
 	
 };
 
